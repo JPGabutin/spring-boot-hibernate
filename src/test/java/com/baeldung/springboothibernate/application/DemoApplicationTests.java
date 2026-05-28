7af1cf63048f8f6d72f11173d9a688c4fd909cb6
jpgab <gabutin.jp@gmail.com>
2026-05-28 17:21:25 +0800

    initial commit

---
+++ src/test/java/com/baeldung/springboothibernate/application/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.springboothibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
