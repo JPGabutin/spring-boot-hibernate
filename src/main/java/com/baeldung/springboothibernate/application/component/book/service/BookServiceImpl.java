0f8b83a13831eeee5b887d3e8c6b5fe85a663535
jpgab <gabutin.jp@gmail.com>
2026-05-30 12:13:35 +0800

    fix: add missing annotations for service and repo layers

--- src/main/java/com/baeldung/springboothibernate/application/component/book/service/BookServiceImpl.java
+++ src/main/java/com/baeldung/springboothibernate/application/component/book/service/BookServiceImpl.java
@@ -2,10 +2,13 @@ package com.baeldung.springboothibernate.application.component.book.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.baeldung.springboothibernate.application.component.book.domain.Book;
 import com.baeldung.springboothibernate.application.component.book.domain.BookRepository;
 import com.baeldung.springboothibernate.application.component.book.domain.BookService;
 
+@Service
 public class BookServiceImpl implements BookService {
     private BookRepository bookRepository;
 
--- src/main/java/com/baeldung/springboothibernate/application/infrastructure/persistence/BookRepositoryImpl.java
+++ src/main/java/com/baeldung/springboothibernate/application/infrastructure/persistence/BookRepositoryImpl.java
@@ -2,9 +2,12 @@ package com.baeldung.springboothibernate.application.infrastructure.persistence;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.baeldung.springboothibernate.application.component.book.domain.Book;
 import com.baeldung.springboothibernate.application.component.book.domain.BookRepository;
 
+@Repository
 public class BookRepositoryImpl implements BookRepository {
     private JpaBookRepository jpaBookRepository;
 
