84cc874a6407c78dcfc581fa7080df5e2d3cfae5
jpgab <gabutin.jp@gmail.com>
2026-05-30 11:57:47 +0800

    fix: align sql import configuration with hibernate

--- src/main/resources/db/import.sql
+++
@@ -1,3 +0,0 @@
-insert into book values('The Tartar Steppe');
-insert into book values('Poem Strip');
-insert into book values('Restless Nights: Selected Stories of Dino Buzzati');
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO book (id, name) VALUES(RANDOM_UUID(), 'The Tartar Steppe');
+INSERT INTO book (id, name) VALUES(RANDOM_UUID(), 'Poem Strip');
+INSERT INTO book (id, name) VALUES(RANDOM_UUID(), 'Restless Nights: Selected Stories of Dino Buzzati');
