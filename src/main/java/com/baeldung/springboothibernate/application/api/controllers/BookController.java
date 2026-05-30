b8705080d47df68a673683d2ab434b2f970b1a1e
jpgab <gabutin.jp@gmail.com>
2026-05-30 12:13:58 +0800

    feat: add book controller

---
+++ src/main/java/com/baeldung/springboothibernate/application/api/controllers/BookController.java
@@ -0,0 +1,27 @@
+package com.baeldung.springboothibernate.application.api.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baeldung.springboothibernate.application.component.book.domain.Book;
+import com.baeldung.springboothibernate.application.component.book.domain.BookService;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> books = bookService.getAllBooks();
+        return ResponseEntity.ok().body(books);
+    }
+}
